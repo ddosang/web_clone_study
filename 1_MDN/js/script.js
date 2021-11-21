d3f41af7f5ff55fef6d1c7c9c3f845226d5c6c26
ddosang <218926@naver.com>
2021-11-21 20:06:01 +0900

    MDN 반응형 js 추가 (#1)

--- 1_MDN/js/script.js
+++ 1_MDN/js/script.js
@@ -1,9 +1,24 @@
 const btns = document.querySelectorAll(".menu__btn");
 const subMenus = document.querySelectorAll(".menu__sub-menu");
 
+const btnSmall = document.querySelector(".btn-menu");
+
 function btnClicked(index) {
     btns[index].classList.toggle("on");
     subMenus[index].classList.toggle("on");
+    
+    for(let i = 0; i < btns.length; i++) {
+        if (i != index) {
+            if (btns[i].classList.contains("on")) {
+                btns[i].classList.toggle("on");
+                subMenus[i].classList.toggle("on");
+            }
+        } 
+    }
+}
+
+function btnSmallCilcked() {
+    btnSmall.classList.toggle("on");
 }
 
 for (let i = 0; i < btns.length; i++) {
@@ -13,4 +28,6 @@ for (let i = 0; i < btns.length; i++) {
         }
     );
 }
+
+btnSmall.addEventListener("click", btnSmallCilcked);
 
